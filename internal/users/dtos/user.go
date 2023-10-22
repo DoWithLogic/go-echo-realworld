7a0f6656ae6a7874514f7df59e079f90cd04cab7
martinyonatann <martin.yonathan305@gmail.com>
2023-10-22 14:02:51 +0700

    chore:remove useless file on users domain

--- internal/users/dtos/user.go
+++ internal/users/dtos/user.go
@@ -1,6 +1,10 @@
 package dtos
 
-import "github.com/invopop/validation"
+import (
+	"github.com/DoWithLogic/go-echo-realworld/pkg/apperror"
+	"github.com/DoWithLogic/go-echo-realworld/pkg/constant"
+	"github.com/invopop/validation"
+)
 
 type (
 	UserRequest struct {
@@ -18,6 +22,22 @@ type (
 		Image    string `json:"image"`
 		Bio      string `json:"bio,"`
 	}
+
+	UpdateUserStatusRequest struct {
+		UserID   int64  `json:"-"`
+		UpdateBy string `json:"-"`
+		Status   int    `json:"status"`
+	}
+
+	UpdateUserRequest struct {
+		UserID      int64  `json:"-"`
+		Fullname    string `json:"fullname"`
+		PhoneNumber string `json:"phone_number"`
+		UserType    string `json:"user_type"`
+		Email       string `json:"email"`
+		Password    string `json:"password"`
+		UpdateBy    string `json:"-"`
+	}
 )
 
 func (x UserRequest) ValidateCreate() error {
@@ -32,5 +52,24 @@ func (x UserRequest) ValidateLogin() error {
 	return validation.ValidateStruct(&x.Data,
 		validation.Field(&x.Data.Email, validation.Required),
 		validation.Field(&x.Data.Password, validation.Required),
+	)
+}
+
+func (ussp UpdateUserStatusRequest) Validate() error {
+	return validation.ValidateStruct(&ussp,
+		validation.Field(&ussp.UserID, validation.NotNil),
+		validation.Field(&ussp.UpdateBy, validation.NotNil),
+		validation.Field(&ussp.Status, validation.Required),
+	)
+}
+
+func (cup UpdateUserRequest) Validate() error {
+	if cup.UserType != "" && cup.UserType != constant.UserTypePremium && cup.UserType != constant.UserTypeRegular {
+		return apperror.ErrInvalidUserType
+	}
+
+	return validation.ValidateStruct(&cup,
+		validation.Field(&cup.UserID, validation.NotNil),
+		validation.Field(&cup.UpdateBy, validation.NotNil),
 	)
 }
--- internal/users/entities/users.go
+++ internal/users/entities/users.go
@@ -2,6 +2,11 @@ package entities
 
 import (
 	"time"
+
+	"github.com/DoWithLogic/go-echo-realworld/config"
+	"github.com/DoWithLogic/go-echo-realworld/internal/users/dtos"
+	"github.com/DoWithLogic/go-echo-realworld/pkg/middleware"
+	"github.com/DoWithLogic/go-echo-realworld/pkg/utils"
 )
 
 type (
@@ -22,3 +27,47 @@ type (
 		PessimisticLocking bool
 	}
 )
+
+func NewUserLogin(res Users, token string) dtos.UserResponse {
+	return dtos.UserResponse{
+		Data: dtos.User{
+			Email: res.Email,
+			Token: token,
+			Bio:   res.Bio,
+			Image: res.Image,
+		},
+	}
+}
+
+func NewCreateUser(data dtos.UserRequest, cfg config.Config) Users {
+	return Users{
+		UserName:  data.Data.UserName,
+		Email:     data.Data.Email,
+		Password:  utils.Encrypt(data.Data.Password, cfg),
+		CreatedAt: time.Now(),
+		CreatedBy: "SYSTEM",
+	}
+}
+func NewUpdateUser(req dtos.UserRequest, cfg config.Config, identity middleware.CustomClaims) Users {
+	return Users{
+		UserID:    identity.UserID,
+		UserName:  req.Data.UserName,
+		Email:     req.Data.Email,
+		Password:  req.Data.Password,
+		Bio:       req.Data.Bio,
+		Image:     req.Data.Image,
+		UpdatedAt: time.Now(),
+		UpdatedBy: identity.Email,
+	}
+}
+
+func NewUserDetail(data Users) dtos.UserResponse {
+	return dtos.UserResponse{
+		Data: dtos.User{
+			Email:    data.Email,
+			UserName: data.UserName,
+			Bio:      data.Bio,
+			Image:    data.Image,
+		},
+	}
+}
